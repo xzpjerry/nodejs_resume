1b88dc1deb9986d4f8f1d82822eeb4ac5d2cd9ee
Jerry Xie <xzpjerry@gmail.com>
2019-04-17 15:42:48 -0700

    Prototype mongodb IO

---
+++ db.js
@@ -0,0 +1,7 @@
+// db.js
+
+module.exports = {
+    // DB: 'mongodb://mongo:27017/nodetest1'
+    DB: 'worker:3789610C-005D-4676-8EAE-C52A73DBFF7B@ds213229.mlab.com:13229/xzpjerryblog',
+    COLLECTION: 'nodetest1'
+}
